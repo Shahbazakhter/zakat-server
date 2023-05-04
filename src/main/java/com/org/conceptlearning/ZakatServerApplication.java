f95fc3faee2c805c0721a2745ecba6f8855835a4
Shahbaz Akhter <akhter.shahbaz31@gmail.com>
2023-05-05 01:46:52 +0530

    Added Swagger http://localhost:8080/swagger-ui/index.html

--- src/main/java/com/org/conceptlearning/ZakatServerApplication.java
+++ src/main/java/com/org/conceptlearning/ZakatServerApplication.java
@@ -1,17 +1,14 @@
 package com.org.conceptlearning;
 
-import com.org.conceptlearning.config.SwaggerConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 import java.io.IOException;
 import java.text.ParseException;
 
 @SpringBootApplication
 @Slf4j
-@Import(SwaggerConfig.class)
 public class ZakatServerApplication {
 
     public static void main(String[] args) throws IOException, ParseException {
